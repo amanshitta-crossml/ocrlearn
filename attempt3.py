94c0defe80c76172852bbb3a3e7f3255bdf3d132
amanshitta-crossml <amanshitta@crossml.com>
2021-09-18 11:32:26 +0530

    attempt3 redefined

--- attempt3.py
+++ attempt3.py
@@ -4,15 +4,23 @@ from nltk import ngrams
 import re
 import pdb
 
-# 0276704000 -- done
-# 0494188000 -- done
-image = cv.imread('Photos/0276704000.tif')
+file_list = ['0276704000.tif','0279062000.tif', '0281217000.tif','0282274000.tif','0494188000.tif','0230551000.tif', '0235100000.tif', \
+    '0240281000.tif','0245808000.tif', '0253651000.tif', '0256958000.tif', '0261918000.tif', \
+        '0264014000.tif', '0269779000.tif', '0273072000.tif']
+print(file_list)
+
+i = input("Enter from index")
+print(file_list[int(i)-1])
+image = cv.imread(f'Photos/{file_list[int(i)-1]}')
+
 image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 image_strings = pytesseract.image_to_string(image)
 
+image_strings = image_strings.replace('\n',' ')
+
 account_pattern = r"\d{2}-\d{5}-\d-\d{2}"
-year_pattern = r'(19|20)[0-9]{2}'
-wages_pattern = r'[0-9][0-9,]*[0-9](\.|\s)?[0-9]{0,2}'
+year_pattern = r'\s(19|20)[0-9]{2}\s'
+wages_pattern = r'[0-9][0-9,]*[0-9](\.|\s)?[0-9]{2}'
 
 final_op = {
     "account": None,
@@ -21,46 +29,28 @@ final_op = {
     
 }
 
-# for key, val in final_op.items():
-#     if val == None:
-sixgrams = ngrams(image_strings.split('\n'), 10)
-
-# pdb.set_trace()
+def find_data_and_dict(key, pattern):
+    sixgrams = ngrams(image_strings.split(), 10)
+    for grams in sixgrams:
+            line = ' '.join(grams)
+            
+            if (key in line.lower()) & (value is None):
+                print(key,line)
+                if re.search(pattern, line):
+                    final_op[key] = re.search(pattern, line).group().strip()
+                    break
 
 for key,value in final_op.items():
-
     if key == 'account':
-        for grams in sixgrams:
-            line = ' '.join(grams)
-            
-            if (key in line.lower()) & (value is None):
-                print(key, line)
-                if re.search(account_pattern, line):
-                    final_op[key] = re.search(account_pattern, line).group()
-                    break
-
+        
+        find_data_and_dict(key, account_pattern)
     elif key == 'year':
-        for grams in sixgrams:
-            line = ' '.join(grams)
-            if (key in line.lower()) & (value is None):
-                print(key, line)
-                if re.search(year_pattern, line.lower()):
-                    
-                    final_op[key] = re.search(year_pattern, line).group()
-                    break
-
+        
+        find_data_and_dict(key, year_pattern)
+       
     elif key == 'wages':
-        for grams in sixgrams:
-            line = ' '.join(grams)
-            if (key in line.lower()) & (value is None):
-                print(key, line)
-                if re.search(wages_pattern, line):
-                    
-                    final_op[key] = re.search(wages_pattern, line).group()
-                    break
-
-    
-            # print(line)
-            # print(key, line)
-            
+        
+        find_data_and_dict(key, wages_pattern)
+        
+                              
 print(final_op)
